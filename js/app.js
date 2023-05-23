1e79d618e326ab62f2a8dac92b1ce617aa532ef4
Ziad-Abazed <ziad.w.abazeed@gmail.com>
2023-05-23 09:04:45 +0000

    edit the generateEmployeeId function to get Unique Id as method

--- js/app.js
+++ js/app.js
@@ -12,9 +12,8 @@ if(arrIds.includes(getID)){
 
 }
 var allEmployees=[]
-function Employee(id ,fullName,department,level,image) {
+function Employee(fullName,department,level,image) {
     
-  this.id = id;
   this.fullName = fullName;
   this.catImage = image;
   this.department = department;
@@ -24,7 +23,9 @@ function Employee(id ,fullName,department,level,image) {
   allEmployees.push(this);
   
 }
-
+Employee.prototype.Id=function(){
+  return generateEmployeeId();
+}
 
 Employee.prototype.salary=function(){
   var salaryRange;
@@ -46,13 +47,13 @@ Employee.prototype.salary=function(){
 }
 
 
-let employee1=new Employee(generateEmployeeId(),"Ghazi Samer","Administration","Senior","https://thumbs.dreamstime.com/b/webcam-view-executive-manager-businessman-having-videocall-employees-216458723.jpg");
-let employee2=new Employee( generateEmployeeId(), 'Lana Ali', 'Finance',  'Senior','https://thumbs.dreamstime.com/b/portrait-happy-office-employee-working-laptop-looking-camera-109745321.jpg' )
-let employee3=new Employee( generateEmployeeId(), 'Tamara Ayoub', 'Marketing',  'Senior','https://thumbs.dreamstime.com/b/office-worker-pretty-young-sitting-41462649.jpg' )
-let employee4=new Employee( generateEmployeeId(), 'Safi Walid', 'Administration',  'Mid-Senior','https://thumbs.dreamstime.com/b/close-up-happy-bearded-man-hr-makes-good-first-impression-close-up-happy-bearded-man-hr-makes-good-first-impression-applicant-183172151.jpg' )
-let employee5=new Employee( generateEmployeeId(), 'Omar Zaid', 'Development',  'Senior','https://www.shutterstock.com/image-photo/portrait-smiling-millennial-arabic-businessman-260nw-1769759789.jpg' )
-let employee6=new Employee( generateEmployeeId(), 'Rana Saleh', 'Development',  'Junior','https://previews.123rf.com/images/wavebreakmediamicro/wavebreakmediamicro1111/wavebreakmediamicro111101651/11205201-busy-office-worker-in-her-office.jpg' )
-let employee7=new Employee( generateEmployeeId(), 'Hadi Ahmad', 'Finance',  'Mid-Senior','https://www.shutterstock.com/image-photo/successful-caucasian-smiling-man-shop-260nw-2079161422.jpg' )
+let employee1=new Employee("Ghazi Samer","Administration","Senior","https://thumbs.dreamstime.com/b/webcam-view-executive-manager-businessman-having-videocall-employees-216458723.jpg");
+let employee2=new Employee(  'Lana Ali', 'Finance',  'Senior','https://thumbs.dreamstime.com/b/portrait-happy-office-employee-working-laptop-looking-camera-109745321.jpg' )
+let employee3=new Employee(  'Tamara Ayoub', 'Marketing',  'Senior','https://thumbs.dreamstime.com/b/office-worker-pretty-young-sitting-41462649.jpg' )
+let employee4=new Employee( 'Safi Walid', 'Administration',  'Mid-Senior','https://thumbs.dreamstime.com/b/close-up-happy-bearded-man-hr-makes-good-first-impression-close-up-happy-bearded-man-hr-makes-good-first-impression-applicant-183172151.jpg' )
+let employee5=new Employee(  'Omar Zaid', 'Development',  'Senior','https://www.shutterstock.com/image-photo/portrait-smiling-millennial-arabic-businessman-260nw-1769759789.jpg' )
+let employee6=new Employee('Rana Saleh', 'Development',  'Junior','https://previews.123rf.com/images/wavebreakmediamicro/wavebreakmediamicro1111/wavebreakmediamicro111101651/11205201-busy-office-worker-in-her-office.jpg' )
+let employee7=new Employee(  'Hadi Ahmad', 'Finance',  'Mid-Senior','https://www.shutterstock.com/image-photo/successful-caucasian-smiling-man-shop-260nw-2079161422.jpg' )
 document.write("<table>")
 document.write("<thead>")
 document.write("<tr>")
@@ -74,7 +75,7 @@ Employee.prototype.render=function(){
   document.write("<tbody>")
   document.write("<tr>")
 
-  document.write(`<td> ${this.id}</td>`)
+  document.write(`<td> ${this.id()}</td>`)
   document.write(`<td> ${this.fullName}</td>`)
   document.write(`<td> ${this.department}</td>`)
   document.write(`<td> ${this.level}</td>`)
